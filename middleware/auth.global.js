da1294f9a556c881844f528c09c355b60faf5df3
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-03 14:03:48 +0900

    cookie 새로고침 개선

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,16 +1,18 @@
 import { useLoginStore } from '@/stores/login' // login.js 에서 useLoginStore 함수 import
 export default function () {
   const loginStore = useLoginStore() // useLoginStore 함수 호출
-  const token = useCookie('accessToken') // 쿠키에서 토큰 가져오기
-  const user = useCookie('accessUser') // 쿠키에서 토큰 가져오기
-  if (token.value && user.value) {
-    loginStore.isLogin = true
-    loginStore.userInfo = user.value
-    console.log('쿠키있음', loginStore.isLogin, loginStore.userInfo)
-  } else {
-    loginStore.isLogin = false
-    loginStore.userInfo.username = null
-    loginStore.userInfo.email = null
-    console.log('쿠키없음', loginStore.isLogin, loginStore.userInfo)
-  }
+  if(loginStore.isLogin === true) {
+    const token = useCookie('accessToken') // 쿠키에서 토큰 가져오기
+    const user = useCookie('accessUser') // 쿠키에서 토큰 가져오기
+    if (token.value && user.value) {
+      loginStore.isLogin = true
+      loginStore.userInfo = user.value
+      console.log('쿠키있음', loginStore.isLogin, loginStore.userInfo)
+    } else {
+      loginStore.isLogin = false
+      loginStore.userInfo.username = null
+      loginStore.userInfo.email = null
+      console.log('쿠키없음', loginStore.isLogin, loginStore.userInfo)
+    }
+  } 
 }
