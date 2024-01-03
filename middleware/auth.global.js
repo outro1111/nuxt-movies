a8cb371e6a4a3f755d49a2fb300d48d8c8930e13
DESKTOP-FVEKAVA\CJ <outro1111@naver.com>
2024-01-03 14:32:49 +0900

    cookie 새로고침 개선

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,12 +1,8 @@
 import { useLoginStore } from '@/stores/login' // login.js 에서 useLoginStore 함수 import
 export default function () {
   const loginStore = useLoginStore() // useLoginStore 함수 호출
-  const token = ref(null) // 토큰 초기화
-  const user = ref(null) // 유저 초기화
-  if (loginStore.isLogin === true) {
-    token.value = useCookie('accessToken') // 쿠키에서 토큰 가져오기
-    user.value = useCookie('accessUser') // 쿠키에서 유저 가져오기
-  }
+  const token = useCookie('accessToken') // 쿠키에서 토큰 가져오기
+  const user = useCookie('accessUser') // 쿠키에서 유저 가져오기
   if (token.value && user.value) {
     loginStore.isLogin = true
     loginStore.userInfo = user.value
